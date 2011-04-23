f801d1caa641eabdb302c0ee24164230f5a31e34
Ruben Sanshis <rusansor@gmail.com>
2011-04-23 10:21:17 +0100

    refactored code.

--- uicontroller/customers.py
+++ uicontroller/customers.py
@@ -7,5 +7,17 @@ class UIControllerCustomer:
         builder = gtk.Builder()
         builder.add_from_file("ui/customers.ui")
         self.window = builder.get_object("window_customer_main")
+
+        self.populate_customers_list(builder)
         builder.connect_signals(self)
         self.window.show()
+
+    def populate_customers_list(self, builder):
+        self.model = builder.get_object("model")
+        self.model.append(['Ruben'])
+        self.model.append(['Natalia'])
+        print self.model
+
+        for row in self.model:
+            value = row[0]
+            print value
