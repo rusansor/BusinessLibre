e94c78d92163fd23f495472cc4d42ae0a751c322
Ruben Sanshis <rusansor@gmail.com>
2011-04-24 12:11:56 +0100

    _

--- BusinessLibre.py
+++ BusinessLibre.py
@@ -5,6 +5,7 @@ import pygtk
 pygtk.require("2.0")
 import gtk
 from uicontroller.customers import UIControllerCustomer
+from uicontroller.products  import UIControllerProducts
 
 import dao.ormsqlalchemy
 
@@ -17,8 +18,11 @@ class App(object):
         builder.connect_signals(self)
         self.window.show()
 
-    def on_menuitem_customer_activate(self,widget):
+    def on_menuitem_customers_activate(self,widget):
         UIControllerCustomer()
+
+    def on_menuitem_products_activate(self,widget):
+        UIControllerProducts()
 
     def on_window_destroy(self, widget, data=None):      
         gtk.main_quit()
---
+++ uicontroller/products.py
@@ -0,0 +1,62 @@
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+from dao.ormsqlalchemy import Session
+from model.product import Product
+
+class UIControllerProducts:
+    def __init__(self):
+        self.session = Session()
+
+        self.builder = gtk.Builder()
+        self.builder.add_from_file("ui/products.ui")
+        self.window = self.builder.get_object("window_product_main")
+        self.listview = self.builder.get_object("listview")
+        self.model = self.builder.get_object("model")
+
+        self.populate_products_list()
+        self.builder.connect_signals(self)
+        self.window.show()
+
+    def populate_products_list(self):
+        self.model = self.builder.get_object("model")
+        self.products = self.session.query(Product).all()
+        for product in self.products:
+            self.model.append([product.id, product.name, product.cif])
+
+    def on_buttonAdd_clicked(self,widget):
+        self.window_edit = self.builder.get_object("window_product_edit")
+        self.window_edit.show()
+
+    def on_buttonEdit_clicked(self,widget):
+        print 'editing...'
+        self.window_edit = self.builder.get_object("window_product_edit")
+        self.window_edit.show()
+
+    def on_buttonDelete_clicked(self,widget):
+        listselection = self.listview.get_selection()
+        model, iter = listselection.get_selected()
+        data = model.get_value(iter, 0)
+        for product in self.products:
+            if product.id == data:
+                self.session.delete(product)
+                self.products.remove(product)
+                self.model.remove(iter)
+                self.session.commit()
+
+    def on_buttonApply_clicked(self,widget):
+        entryName = self.builder.get_object("entryName")
+        name = entryName.get_text()
+        entryCIF = self.builder.get_object("entryCIF")
+        cif = entryCIF.get_text()
+        newProduct = Product(None, name, cif)
+        self.addProduct(newProduct)
+        self.window_edit.destroy()
+
+    def addProduct(self, product):
+        self.session.add(product)
+        self.session.commit()
+        self.model.append([product.id, product.name, product.cif])
+
+
